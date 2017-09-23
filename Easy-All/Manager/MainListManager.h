28e1d9264a8eafaf15c7c871f275ff555e1618b6
wind4app <chensa@wind4app.com>
2017-09-23 09:40:14 +0800

    1.新建主页面数据管理

---
+++ Easy-All/Manager/MainListManager.h
@@ -0,0 +1,13 @@
+//
+//  MainListManager.h
+//  Easy-All
+//
+//  Created by chensa on 2017/9/23.
+//  Copyright © 2017年 chensa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MainListManager : NSObject
+
+@end
---
+++ Easy-All/Manager/MainListManager.m
@@ -0,0 +1,13 @@
+//
+//  MainListManager.m
+//  Easy-All
+//
+//  Created by chensa on 2017/9/23.
+//  Copyright © 2017年 chensa. All rights reserved.
+//
+
+#import "MainListManager.h"
+
+@implementation MainListManager
+
+@end
