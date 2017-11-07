78436aa3df791bad4bd19fa5310b67e457a23fa5
Periphery992 <chencode992@163.com>
2017-11-07 20:58:01 +0800

    加入UserDefaults关键字管理
    加入UserDefaults关键字管理

---
+++ Easy-All/Manager/HLFUserDefaults/UserDefaultsKey.h
@@ -0,0 +1,19 @@
+
+//
+//  UserDefaultsKey.h
+//  fairy-kingdom
+//
+//  Created by chen on 30/10/2017.
+//  Copyright © 2017 chen. All rights reserved.
+//
+
+#ifndef UserDefaultsKey_h
+#define UserDefaultsKey_h
+
+#define USKey_HttpUrl   @"USKey_HttpUrl"
+
+#define USKey_Account   @"USKey_Account"
+
+
+
+#endif /* UserDefaultsKey_h */
